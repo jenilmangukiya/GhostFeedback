7b86c694ef68bd2191ca755314bf55c29e43cb28
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-08-09 22:19:44 +0530

    Feat: accept messages API

---
+++ src/app/api/accept-messages/route.ts
@@ -0,0 +1,109 @@
+import dbConnect from "@/lib/dbConnect";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
+import { UserModel } from "@/models/user.model";
+
+export const POST = async (request: Request) => {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const sessionUser = session?.user;
+
+  if (!session || !sessionUser) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized Request",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const { acceptMessage } = await request.json();
+
+    const updatedUser = await UserModel.findOneAndUpdate(
+      {
+        _id: sessionUser._id,
+      },
+      {
+        isAcceptingMessage: acceptMessage,
+      },
+      {
+        new: true,
+      }
+    );
+    if (!updatedUser) {
+      return Response.json(
+        {
+          success: false,
+          message: "Failed to update status of user accepting the message",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+
+    return Response.json({
+      success: true,
+      message: "User successfully updated for accepting the messages",
+    });
+  } catch (error) {
+    console.error("Error while setting up the accept message status :", error);
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized Request",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+};
+
+export const GET = async (request: Request) => {
+  await dbConnect();
+
+  const session = await getServerSession();
+
+  const sessionUser = session?.user;
+  if (!session || !sessionUser) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized request",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const user = await UserModel.findById(sessionUser._id);
+
+  if (!user) {
+    return Response.json(
+      {
+        success: false,
+        message: "User not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  return Response.json(
+    {
+      success: true,
+      isAcceptingMessages: user.isAcceptingMessage,
+    },
+    {
+      status: 200,
+    }
+  );
+};
