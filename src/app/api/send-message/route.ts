607281dcc34ebe094de737e0e507a716068280e5
jenilmangukiya <jenilmangukiya2002@gmail.com>
2024-08-09 23:22:46 +0530

    Feat: API for Send messages

---
+++ src/app/api/send-message/route.ts
@@ -0,0 +1,59 @@
+import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/options";
+import { Message, UserModel } from "@/models/user.model";
+
+export const POST = async (request: Request) => {
+  await dbConnect();
+
+  const session = await getServerSession(authOptions);
+  const sessionUser = session?.user;
+
+  if (!session || !sessionUser) {
+    return Response.json(
+      {
+        success: false,
+        message: "Unauthorized Request",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  try {
+    const user = await UserModel.findById(sessionUser._id);
+    if (!user) {
+      return Response.json({
+        success: false,
+        message: "User not found",
+      });
+    }
+
+    if (!user.isAcceptingMessage) {
+      return Response.json({
+        success: false,
+        message:
+          "Failed to send message to the user, User is not accepting the messages at the moment",
+      });
+    }
+    const { content } = await request.json();
+
+    const newMessage = { content, createdAt: new Date() };
+
+    user.messages.push(newMessage as Message);
+
+    const updatedUser = await user.save();
+    if (!updatedUser) {
+      return Response.json({
+        success: false,
+        message: "Failed to save the message",
+      });
+    }
+
+    return Response.json({
+      success: true,
+      message: "Successfully send the message to the user ",
+    });
+  } catch (error) {}
+};
